d7a967349117b1b1726ec93239706f01b11da460
puran30379495 <105132463+puran30379495@users.noreply.github.com>
2022-06-06 19:13:19 +1000

    Add files via upload

---
+++ adminIndex.php
@@ -0,0 +1,165 @@
+
+<?php 
+session_start();
+
+   include("connection.php");
+   include("function.php");
+
+   $user_data = check_adminlogin($con);
+
+   if(isset($_POST['add_product'])){
+
+   $product_name = $_POST['product_name'];
+   $product_price = $_POST['product_price'];
+   $product_image = $_FILES['product_image']['name'];
+   $product_image_tmp_name = $_FILES['product_image']['tmp_name'];
+   $product_image_folder = 'uploaded_img/'.$product_image;
+   $user_id = $user_data['user_id'];
+
+   if(empty($product_name) || empty($product_price) || empty($product_image)){
+      $message[] = 'please fill out all';
+   }else{
+      $insert = "insert into productdetails (user_id,name, price, image) VALUES('$user_id','$product_name', '$product_price', '$product_image')";
+      $upload = mysqli_query($con,$insert);
+      if($upload){
+         move_uploaded_file($product_image_tmp_name, $product_image_folder);
+         $message[] = 'new product added successfully';
+      }else{
+         $message[] = 'could not add the product';
+      }
+   }
+
+};
+
+if(isset($_GET['delete'])){
+   $id = $_GET['delete'];
+   mysqli_query($con, "DELETE FROM productdetails WHERE id = $id");
+   header('location:adminIndex.php');
+};
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Admin page</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style2.css">
+
+</head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<style>
+  html{
+    font-size: 10px;
+  }
+body {margin: 0;}
+
+ul.topnav {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+  position: -webkit-sticky; /* Safari */
+  position: sticky;
+  top: 0;
+}
+
+ul.topnav li {float: left;}
+
+ul.topnav li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+ul.topnav li a:hover:not(.active) {background-color: #111;}
+
+ul.topnav li a.active {background-color: #04AA6D;}
+
+ul.topnav li.right {float: right;}
+
+@media screen and (max-width: 600px) {
+  ul.topnav li.right, 
+  ul.topnav li {float: none;}
+}
+</style>
+</head>
+<body>
+
+<ul class="topnav">
+  <li><a class="active" href="adminIndex.php">Home</a></li>
+  <li><a href="viewOrders.php">Order</a></li>
+  <li><a href="roasterUpdate.php">Roaster</a></li>
+  <li class="right"><a href="logout.php">Logout</a></li>
+</ul>
+
+<?php
+
+if(isset($message)){
+   foreach($message as $message){
+      echo '<span class="message">'.$message.'</span>';
+   }
+}
+
+?>
+   
+<div class="container">
+
+   <div class="admin-product-form-container">
+
+      <form action="<?php $_SERVER['PHP_SELF'] ?>" method="post" enctype="multipart/form-data">
+         <h3>add a new product</h3>
+         <input type="text" placeholder="enter product name" name="product_name" class="box">
+         <input type="number" placeholder="enter product price" name="product_price" class="box">
+         <input type="file" accept="image/png, image/jpeg, image/jpg" name="product_image" class="box">
+         <input type="submit" class="btn" name="add_product" value="add product">
+      </form>
+
+   </div>
+
+   <?php
+
+   $select = mysqli_query($con, "SELECT * FROM productdetails");
+   
+   ?>
+   <div class="product-display">
+      <table class="product-display-table">
+         <thead>
+         <tr>
+            <th>product image</th>
+            <th>product name</th>
+            <th>product price</th>
+            <th>action</th>
+         </tr>
+         </thead>
+         <?php while($row = mysqli_fetch_assoc($select)){ ?>
+         <tr>
+            <td><img src="uploaded_img/<?php echo $row['image']; ?>" height="100" alt=""></td>
+            <td><?php echo $row['name']; ?></td>
+            <td>$<?php echo $row['price']; ?>/-</td>
+            <td>
+               <a href="adminUpdate.php?edit=<?php echo $row['id']; ?>" class="btn"> <i class="fas fa-edit"></i> edit </a>
+               <a href="adminIndex.php?delete=<?php echo $row['id']; ?>" class="btn"> <i class="fas fa-trash"></i> delete </a>
+            </td>
+         </tr>
+      <?php } ?>
+      </table>
+   </div>
+
+</div>
+
+</body>
+</html>
+
+
